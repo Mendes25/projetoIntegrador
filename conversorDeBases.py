3b1d2e33cd181c2b19e81cb621ed3d89f02f55a5
Mendes <daniel.mendes01.ti@gmail.com>
2024-04-24 18:58:35 -0300

    V1.0 - Versão incompleta

---
+++ conversorDeBases.py
@@ -0,0 +1,54 @@
+# converta número da base decimal para hexadecimal 
+# converta número da base hexadecimal para decimal
+# reverse
+# print(12412%16) é o que sobra da divisão, aqui o resultado é 12 
+# print(12412//16) é a quantidade de vezes em que o 16 foi multiplicado, aqui seria 775
+
+numero=input("Insira o número que você deseja: ")
+
+def conversao_decimal_para_hexadecimal(numero): 
+    hexadecimalList=[]
+    numeroDict={
+        10:"a",
+        11:"b",
+        12:"c",
+        13:"d",
+        14:"e",
+        15:"d"
+    }
+    
+    numero=int(numero)
+
+    while numero <0:
+        numero//16
+        print(numero)
+conversao_decimal_para_hexadecimal(numero)
+
+
+        
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
